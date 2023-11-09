81f1b81b26dec744d2139d615471ee19a752e165
KarsaiArtur <127442850+KarsaiArtur@users.noreply.github.com>
2023-11-09 18:59:26 +0100

    5.2

--- RepMaster/src/main/java/tracker/model/Rating.java
+++ RepMaster/src/main/java/tracker/model/Rating.java
@@ -1,4 +1,4 @@
-package tracker.model;
+/*package tracker.model;
 
 import jakarta.persistence.*;
 import lombok.EqualsAndHashCode;
@@ -64,4 +64,75 @@ public class Rating {
         }
         this.rating = sum / (double)ratingsCnt;
     }
+}*/
+
+package tracker.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Getter
+@Setter
+@EqualsAndHashCode(of = "id")
+@NoArgsConstructor
+@Entity
+public class Rating {
+    @Id
+    @GeneratedValue
+    private int id;
+
+    //@OneToMany(mappedBy = "rating_")
+    //private List<RatingComment> allRatings;
+
+    private double rating;
+    private String comment;
+
+    @ManyToOne
+    private Exercise exercise;
+    @ManyToOne
+    private Gym gym;
+    @ManyToOne
+    private Workout workout;
+
+
+    public Rating(double rating, String comment){
+        this.rating = rating;
+        this.comment = comment;
+    }
+    /*@Getter
+    @Setter
+    @EqualsAndHashCode(of = "id")
+    @Entity
+    public class RatingComment{
+        @Id
+        @GeneratedValue
+        private int id;
+        String comment;
+        int rating;
+        @ManyToOne
+        private Rating rating_;
+
+        public RatingComment(String comment, int rating){
+            this.comment = comment;
+            this.rating = rating;
+        }
+
+        public RatingComment() {
+
+        }
+    }*/
+
+    /*public void calculateRating() {
+        int ratingsCnt = 0;
+        int sum = 0;
+        for(RatingComment currentRating : allRatings) {
+            ratingsCnt++;
+            sum += currentRating.getRating();
+        }
+        this.rating = sum / (double)ratingsCnt;
+    }*/
 }
+
