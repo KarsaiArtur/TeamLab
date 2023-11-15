ee85a766415e0a65c0276c6cc15dadc9d4d23d60
KarsaiArtur <127442850+KarsaiArtur@users.noreply.github.com>
2023-11-15 12:00:09 +0100

    tdd

--- RepMaster/src/test/java/tracker/service/RegisteredUserTestIT.java
+++ RepMaster/src/test/java/tracker/service/RegisteredUserTestIT.java
@@ -1,13 +1,21 @@
 package tracker.service;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.context.SpringBootTest;
 import tracker.model.Rating;
+import tracker.model.RegisteredUser;
 
 @SpringBootTest
 @AutoConfigureTestDatabase
 public class RegisteredUserTestIT {
+    @Autowired
+    private RegisteredUserService registeredUserService;
 
-
+    @Test
+    public void canAddRegisteredUserToDB() throws Exception{
+        RegisteredUser rUser = new RegisteredUser();
+        registeredUserService.addRegistereduser(rUser);
+    }
 }
